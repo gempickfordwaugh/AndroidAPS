5c00613d3c993681a80e6e7532c7676a7c41ccae
Milos Kozak <m.kozak@sysop.cz>
2017-05-24 18:24:23 +0200

    combo plugin skeleton

--- app/src/main/java/info/nightscout/androidaps/Config.java
+++ app/src/main/java/info/nightscout/androidaps/Config.java
@@ -12,6 +12,7 @@ public class Config {
     public static final boolean WEAR = BuildConfig.WEAR;
 
 
+    public static final boolean COMBO = true && BuildConfig.PUMPDRIVERS;
     public static final boolean DANAR = true && BuildConfig.PUMPDRIVERS;
     public static final boolean DANARv2 = true;
 
@@ -19,7 +20,6 @@ public class Config {
     public static final boolean VIRTUALPUMP = !BuildConfig.NSCLIENTOLNY;
     public static final boolean MDI = !BuildConfig.NSCLIENTOLNY;
     public static final boolean OTHERPROFILES = !BuildConfig.NSCLIENTOLNY;
-    public static final boolean TEMPBASALS = !BuildConfig.NSCLIENTOLNY;
     public static final boolean SAFETY = !BuildConfig.NSCLIENTOLNY;
 
     public static final boolean SMSCOMMUNICATORENABLED = !BuildConfig.NSCLIENTOLNY;
